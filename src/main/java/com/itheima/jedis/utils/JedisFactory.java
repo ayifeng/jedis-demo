c50a7101a93838f831ca5923cdad29d95d2af977
happy <2892696942@qq.com>
2022-04-23 12:19:01 +0800

    github 推送测试

--- src/main/java/com/itheima/jedis/utils/JedisFactory.java
+++ src/main/java/com/itheima/jedis/utils/JedisFactory.java
@@ -32,5 +32,6 @@ public class JedisFactory {
         System.out.println("测试一下git2");
         System.out.println("测试一下git------------------");
         System.out.println("测试一下git2");
+        System.out.println("测试一下往github推送");
     }
 }
